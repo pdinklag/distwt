ade74a26be808e6436fe74e27a65cab43709abb6
Patrick Dinklage <patrick.dinklage@tu-dortmund.de>
2018-08-29 09:31:56 +0200

    WT verification

--- distwt/src/distwt.cpp
+++ distwt/src/distwt.cpp
@@ -15,6 +15,7 @@
 #include <thrill/api/reduce_by_key.hpp>
 #include <thrill/api/size.hpp>
 #include <thrill/api/sort.hpp>
+#include <thrill/api/sum.hpp>
 #include <thrill/api/write_lines_one.hpp>
 #include <thrill/api/zip.hpp>
 
@@ -24,13 +25,14 @@ using esym_t = unsigned short;
 using HistEntry = std::pair<sym_t, size_t>;
 using EAMap = std::unordered_map<sym_t, esym_t>;
 
+using wt_t = std::vector<thrill::DIA<bool>>;
+
 std::ostream& operator << (std::ostream& os, const HistEntry& p) {
     return os << p.first << " -> " << p.second;
 }
 
 template<typename InputDIA>
-std::vector<thrill::DIA<bool>> ConstructWT_StableSort(
-    const InputDIA& input, const size_t sigma) {
+wt_t ConstructWT_StableSort(const InputDIA& input, const size_t sigma) {
 
     // compute size of WT
     const size_t wt_height = tlx::integer_log2_ceil(sigma-1);
@@ -55,6 +57,7 @@ std::vector<thrill::DIA<bool>> ConstructWT_StableSort(
             text = text
                 .SortStable(
                     [&](esym_t a, esym_t b){
+                        // stably sort according to newest bit
                         return (a >> rsh) < (b >> rsh);
                     })
                 .Execute() // do it NOW
@@ -63,6 +66,71 @@ std::vector<thrill::DIA<bool>> ConstructWT_StableSort(
     }
 
     return wt;
+}
+
+thrill::DIA<sym_t> DecodeWT(
+    thrill::Context& ctx,
+    const wt_t& wt,
+    const std::vector<HistEntry>& hist) {
+
+    using esym_index_t = std::pair<esym_t, size_t>;
+
+    // reconstruct symbols from bit vector
+    const size_t n = wt[0].Size();
+    auto xtext = thrill::api::Generate(ctx, n, [](size_t i){
+        // construct indexed sequence of 0 symbols
+        return esym_index_t(0, i);}
+    );
+
+    const size_t wt_height = wt.size();
+    for(size_t level = 0; level < wt_height; level++) {
+        const size_t lsh = wt_height - 1 - level;
+
+        xtext = wt[level]
+            .Zip(xtext, [lsh](bool bit, esym_index_t x) {
+                // OR symbols using current vector
+                return esym_index_t(x.first | (bit << lsh), x.second);
+            })
+            .SortStable([lsh](esym_index_t a, esym_index_t b){
+                // stably reorder according to newest bit
+                return (a.first >> lsh) < (b.first >> lsh);
+            })
+            .Execute() // do it NOW
+            .Collapse();
+    }
+
+    // restore original text
+    auto text = xtext
+        .SortStable([](esym_index_t a, esym_index_t b){
+            // stably sort back by original index
+            return a.second < b.second;
+        })
+        .Map([](esym_index_t x){
+            // map indexed pair back to effective symbol
+            return x.first;
+        })
+        .Map([hist](esym_t x){
+            // undo effective transformation using histogram
+            return hist[x].first;
+        });
+
+    return text.Cache();
+}
+
+template<typename ValueType>
+size_t Compare(
+    const thrill::DIA<ValueType>& a,
+    const thrill::DIA<ValueType>& b) {
+
+    return a
+        .Zip(b, [](const ValueType& a, const ValueType& b){
+            // test if single items are equal
+            return (a != b) ? 1ULL : 0ULL;
+        })
+        .Sum([](size_t a, size_t b){
+            // sum up amount of differing items (diff)
+            return a + b;
+        }, 0ULL);
 }
 
 void Process(thrill::Context& ctx, std::string input) {
@@ -100,6 +168,21 @@ void Process(thrill::Context& ctx, std::string input) {
     // print WT
     for(size_t i = 0; i < wt.size(); i++) {
         wt[i].Print(std::string("wt_") + std::to_string(i+1));
+    }
+
+    // decode WT
+    auto decoded = DecodeWT(ctx, wt, hist);
+
+    // compare raw and decoded texts as a means to verify the WT
+    const size_t diff = Compare(rawtext, decoded);
+
+    // output result on first worker
+    if(ctx.my_rank() == 0) {
+        if(diff == 0) {
+            std::cout << "WT verification succeeded!" << std::endl;
+        } else {
+            std::cout << "WT verification FAILED (diff = " << diff << ")!" << std::endl;
+        }
     }
 }
 
