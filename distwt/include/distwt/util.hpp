f135c2227490629b6a16e9ae905e42b45f27c4e0
Patrick Dinklage <patrick.dinklage@tu-dortmund.de>
2018-10-16 09:31:31 +0200

    Experimental reduction method

--- distwt/include/distwt/util.hpp
+++ distwt/include/distwt/util.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <fstream>
+#include <vector>
 
 namespace util {
 
@@ -9,4 +10,15 @@ inline size_t file_size(const std::string& filename) {
     return in.tellg();
 }
 
+}
+
+// stream output for bit vectors
+inline std::ostream& operator<<(
+    std::ostream& os, const std::vector<bool>& bv) {
+
+    for(bool b : bv) {
+        os << b ? '1' : '0';
+    }
+
+    return os;
 }
