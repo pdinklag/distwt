69a56142ee3f4ef3d87e324b0e57121fed9550c6
Patrick Dinklage <patrick.dinklage@tu-dortmund.de>
2018-11-09 09:46:12 +0100

    Change file_size to use stat

--- distwt/include/distwt/common/util.hpp
+++ distwt/include/distwt/common/util.hpp
@@ -3,11 +3,14 @@
 #include <fstream>
 #include <vector>
 
+#include <sys/stat.h>
+
 namespace util {
 
 inline size_t file_size(const std::string& filename) {
-    std::ifstream in(filename, std::ifstream::ate | std::ifstream::binary);
-    return in.tellg();
+    struct stat buf;
+    stat(filename.c_str(), &buf);
+    return buf.st_size;
 }
 
 }
