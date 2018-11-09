5d059074820809ab59bf019a9053c6d6d152638a
Patrick Dinklage <patrick.dinklage@tu-dortmund.de>
2018-11-09 12:23:38 +0100

    Use tlx::CmdLineParser

--- distwt/src/mpi-apps/hist.cpp
+++ distwt/src/mpi-apps/hist.cpp
@@ -1,3 +1,5 @@
+#include <tlx/cmdline_parser.hpp>
+
 #include <distwt/common/util.hpp>
 #include <distwt/mpi/context.hpp>
 #include <distwt/mpi/file_partition_reader.hpp>
@@ -7,19 +9,24 @@ int main(int argc, char** argv) {
     MPIContext ctx(&argc, &argv);
 
     // Read command-line
-    if(argc < 2) {
-        ctx.cout(ctx.rank() == 0) << "Usage: "
-            << argv[0] << " FILE" << std::endl;
+    tlx::CmdlineParser cp;
 
+    size_t memory = 268'435'456ULL; // default to 256 MiB
+    cp.add_bytes('m', "mem", memory, "Amount of available local memory.");
+
+    std::string input_filename; // reuquired
+    cp.add_param_string("file", input_filename, "The input file.");
+    if (!cp.process(argc, argv, ctx.cout(ctx.rank() == 0))) {
         return -1;
     }
 
-    FilePartitionReader input(ctx, argv[1]);
+    // Count local histogram
+    FilePartitionReader input(ctx, input_filename);
 
     ctx.cout() << "Process partial text: ";
     input.process_local([&](unsigned char c){
         std::cout << c;
-    }, 4194304ULL /* 4 MiB */);
+    }, memory);
     std::cout << std::endl;
     return 0;
 }
