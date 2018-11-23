4f1fbd2c93cbac400c056f5c9666605aec2bb628
Patrick Dinklage <patrick.dinklage@tu-dortmund.de>
2018-11-23 16:46:54 +0100

    Fix size of effective symbol (esym_t) ...

--- include/distwt/common/effective_alphabet.hpp
+++ include/distwt/common/effective_alphabet.hpp
@@ -6,39 +6,8 @@
 
 class EffectiveAlphabetBase {
 public:
-    // an entry index into the effective alphabet
-    using index_t = unsigned char;
-
-    // a symbol within an effective alphabet
-    struct symbol_t {
-    private:
-        index_t m_idx;
-
-    public:
-        inline symbol_t(){}
-
-        inline symbol_t(index_t idx)       : m_idx(idx){}
-        inline symbol_t(const symbol_t& x) : m_idx(x.m_idx){}
-        inline symbol_t(symbol_t&& x)      : m_idx(x.m_idx){}
-
-        inline operator index_t() const { return m_idx; }
-
-        inline symbol_t& operator =(const symbol_t& x) {
-            m_idx = x.m_idx;
-            return *this;
-        }
-
-        inline symbol_t& operator =(index_t idx) {
-            m_idx = idx;
-            return *this;
-        }
-
-        inline bool operator <(const symbol_t& x) const {
-            return m_idx < x.m_idx;
-        }
-
-        const index_t& index = m_idx;
-    };
+    // a symbol in the effective alphabet
+    using symbol_t = unsigned char;
 
 protected:
     std::unordered_map<HistogramBase::symbol_t, symbol_t> m_map;
@@ -49,11 +18,3 @@ public:
 };
 
 using esym_t  = EffectiveAlphabetBase::symbol_t;
-
-// stream output for esym_t
-inline std::ostream& operator<<(
-    std::ostream& os, const esym_t& x) {
-
-    os << size_t(x.index);
-    return os;
-}
--- include/distwt/thrill/effective_alphabet.hpp
+++ include/distwt/thrill/effective_alphabet.hpp
@@ -15,17 +15,3 @@ public:
 
 // convenience aliases
 using etext_t = EffectiveAlphabet::text_t;
-
-// serialization for EffectiveAlphabet::symbol_t
-template<typename Archive>
-struct thrill::data::Serialization<Archive, esym_t>{
-    static void Serialize(const esym_t& x, Archive& ar) {
-        ar.PutRaw(x.index);
-    }
-    static esym_t Deserialize(Archive& ar) {
-        return esym_t(ar.template GetRaw<EffectiveAlphabet::index_t>());
-    }
-    static constexpr bool   is_fixed_size = true;
-    static constexpr size_t fixed_size    = sizeof(EffectiveAlphabet::index_t);
-};
-
