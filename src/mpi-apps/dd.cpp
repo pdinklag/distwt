526885100f03f0dc8fda94b8743cd26021b46a5b
Patrick Dinklage <patrick.dinklage@tu-dortmund.de>
2018-11-30 08:12:29 +0100

    Extract sequential WT construction

--- src/mpi-apps/dd.cpp
+++ src/mpi-apps/dd.cpp
@@ -8,81 +8,14 @@
 #include <distwt/mpi/context.hpp>
 #include <distwt/mpi/file_partition_reader.hpp>
 
-#include <distwt/common/wt.hpp>
 #include <distwt/mpi/histogram.hpp>
 #include <distwt/mpi/effective_alphabet.hpp>
 #include <distwt/mpi/bit_vector.hpp>
+#include <distwt/mpi/wt_construct_sequential.hpp>
 #include <distwt/mpi/wt_nodebased.hpp>
 #include <distwt/mpi/wt_levelwise.hpp>
 
 #include <distwt/mpi/result.hpp>
-
-void recursiveWT(
-    WaveletTree::bits_t& bits,
-    const MPIContext& ctx,
-    const size_t node_id,
-    esym_t* text,
-    const size_t n,
-    const size_t a,
-    const size_t b,
-    esym_t* buffer) {
-
-    if(a == b) return;
-
-    const size_t m = (a + b) / 2;
-
-    // compute node bit vector
-    size_t z = 0;
-    {
-        auto& bv = bits[node_id-1];
-        bv.resize(n);
-
-        for(size_t i = 0; i < n; i++) {
-            if(text[i] <= m) {
-                bv[i] = 0;
-                ++z;
-            } else {
-                bv[i] = 1;
-            }
-        }
-    }
-
-    // only continue if needed
-    if(a < m || m+1 < b) {
-        // compute permutation of text in buffer
-        {
-            esym_t* pl = buffer;
-            esym_t* pr = buffer + z;
-
-            for(size_t i = 0; i < n; i++) {
-                auto c = text[i];
-                if(c <= m) {
-                    *pl++ = c;
-                } else {
-                    *pr++ = c;
-                }
-            }
-        }
-
-        // recurse on left part
-        recursiveWT(
-            bits,
-            ctx,
-            2ULL * node_id, // left child
-            buffer, z,
-            a, m,
-            text);
-
-        // recurse on right part
-        recursiveWT(
-            bits,
-            ctx,
-            2ULL * node_id + 1ULL, // right child
-            buffer + z, n - z,
-            m+1, b,
-            text + z);
-    }
-}
 
 int main(int argc, char** argv) {
     // Read command-line
@@ -150,7 +83,7 @@ int main(int argc, char** argv) {
         bits.resize(wt.num_nodes());
 
         esym_t* buffer = new esym_t[local_num];
-        recursiveWT(
+        wt_construct_nodebased_sequential(
             bits,
             ctx,
             1ULL, // root
