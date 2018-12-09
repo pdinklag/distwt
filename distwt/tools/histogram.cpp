acb9cd5fa65284452d20d64fc5dca00f968a5ecd
Patrick Dinklage <patrick.dinklage@tu-dortmund.de>
2018-12-09 09:53:32 +0100

    Add histogram tool for debugging purposes

---
+++ distwt/tools/histogram.cpp
@@ -0,0 +1,58 @@
+#include <array>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+
+#include <tlx/cmdline_parser.hpp>
+
+constexpr size_t N = 256;
+
+int main(int argc, const char** argv) {
+    // Read command-line
+    tlx::CmdlineParser cp;
+    cp.set_description("Computes and prints the histogram of the given file");
+    cp.set_author("Patrick Dinklage <patrick.dinklage@udo.edu>");
+
+    std::string filename; // required
+    cp.add_param_string("file", filename, "The input file.");
+
+    if (!cp.process(argc, argv)) {
+        return -1;
+    }
+
+    // compute histogram
+    size_t total = 0;
+    std::array<size_t, N> hist{};
+    {
+        std::ifstream in(filename, std::ios::binary);
+
+        while(true) {
+            unsigned char c;
+            in.get((char&)c);
+            if(in.eof()) break;
+
+            ++hist[c];
+            ++total;
+        }
+    }
+
+    // print histogram
+    std::cout << "Read " << total << " bytes in total. Histogram:" << std::endl;
+    for(size_t i = 0; i < N; i++) {
+        if(hist[i] > 0) {
+            std::cout << "   0x" << std::setw(2) << std::setfill('0')
+                << std::uppercase << std::hex << i;
+
+            switch(i) {
+                case 0x9: std::cout << " ( \\t)"; break;
+                case 0xA: std::cout << " ( \\n)"; break;
+                case 0xD: std::cout << " ( \\r)"; break;
+                default:  std::cout << " ('" << (char)i << "')"; break;
+            }
+
+            std::cout << ": " << std::dec << hist[i] << std::endl;
+        }
+    }
+
+    return 0;
+}
