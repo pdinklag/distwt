1c0342ada0cc6970dbc6973cf8b7b2dba75149d6
Patrick Dinklage <patrick.dinklage@tu-dortmund.de>
2019-07-18 14:21:53 +0200

    Framework for histogram impl for large alphabets

--- distwt/mpi/histogram.cpp
+++ distwt/mpi/histogram.cpp
@@ -1,16 +1,44 @@
+#include <unordered_map>
+
 #include <distwt/mpi/histogram.hpp>
 
 #include <distwt/common/util.hpp>
 #include <distwt/mpi/context.hpp>
 #include <distwt/mpi/file_partition_reader.hpp>
 
-// FIXME: what to do for sigma >= 256 ??
-constexpr size_t SIGMA_MAX = 256ULL;
-
-Histogram::Histogram(
+// for arbritrary alphabets
+template<typename T>
+void compute_histogram(
+    std::vector<HistogramBase::entry_t>& m_entries,
     MPIContext& ctx,
     const FilePartitionReader& input,
     const size_t rdbufsize) {
+
+    // compute local histogram
+    std::unordered_map<symbol_t, size_t> local_hist;
+    
+    input.process_local([&](symbol_t c){
+        auto it = local_hist.find(c);
+        if(it != local_hist.end()) {
+            ++it->second;
+        } else {
+            local_hist.emplace(c, 1);
+        }
+    }, rdbufsize);
+
+    // --- TODO: distribute using tree-like communication ---
+    // ...
+}
+
+// for 8-bit alphabets
+template<>
+void compute_histogram<unsigned char>(
+    std::vector<HistogramBase::entry_t>& m_entries,
+    MPIContext& ctx,
+    const FilePartitionReader& input,
+    const size_t rdbufsize) {
+
+    constexpr size_t SIGMA_MAX = 256ULL;
 
     // compute local histogram
     size_t local_hist[SIGMA_MAX] = {0};
@@ -28,4 +56,12 @@ Histogram::Histogram(
             m_entries.emplace_back((symbol_t)c, hist[c]);
         }
     }
+}
+
+Histogram::Histogram(
+    MPIContext& ctx,
+    const FilePartitionReader& input,
+    const size_t rdbufsize) {
+
+    compute_histogram<symbol_t>(m_entries, ctx, input, rdbufsize);
 }
