804630a62e3babc8948a51067015326d57123613
Patrick Dinklage <patrick.dinklage@tu-dortmund.de>
2019-07-19 11:14:06 +0200

    Add mpi_type definitions

--- distwt/mpi/mpi_type.hpp
+++ distwt/mpi/mpi_type.hpp
@@ -8,6 +8,14 @@ template<> struct mpi_type<unsigned char> {
     static constexpr MPI_Datatype id() { return MPI_BYTE; }
 };
 
+template<> struct mpi_type<unsigned short> {
+    static constexpr MPI_Datatype id() { return MPI_SHORT; }
+};
+
+template<> struct mpi_type<unsigned int> {
+    static constexpr MPI_Datatype id() { return MPI_INT; }
+};
+
 template<> struct mpi_type<uint64_t> {
     static constexpr MPI_Datatype id() { return MPI_LONG_LONG; }
 };
